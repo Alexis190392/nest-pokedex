3df571c1a577418ae87578f1a86bf875fd5bb1fe
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-02-16 21:12:26 -0300

    Fix Insertar multiples registros

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -16,18 +16,28 @@ export class SeedService {
       ) {}
   
   async executeSeed(){
-    const {data} = await this.axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=250");
     
-    //extraer el id
+    await this.pokemonModel.deleteMany({}); //elimino todos los pokemon de la bd
+    
+    const {data} = await this.axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=250");
+    const insertPromisesArray = [];
+    
+      //extraer el id
     data.results.forEach(async ({name,url}) => {
+      
       //divido por slash
       const segments = url.split('/');
       const no: number = +segments[segments.length-2];
       console.log({name,no});
       
-      const pokemon = await this.pokemonModel.create({name,no});
+      // const pokemon = await this.pokemonModel.create({name,no});
+      insertPromisesArray.push(
+          this.pokemonModel.create({name,no})
+      );
       
     })
+    
+    await Promise.all(insertPromisesArray);// hace las insersiones de forma simultanea una vez
     
     // return data.results;
     return "SEED EJECUTADO";
