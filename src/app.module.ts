2c5562a5504417664e80b73c18f72351da8295d6
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-02-19 09:39:54 -0300

    Agrego Joi Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import { SeedModule } from './seed/seed.module';
 import * as process from "process";
 import { ConfigModule } from "@nestjs/config";
 import { EnvConfiguration } from "./config/app.config";
+import { JoiValidationSchema } from "./config/joi.validation";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema, //puede trabajar en conjunto con el load
     }),
     
     ServeStaticModule.forRoot({
